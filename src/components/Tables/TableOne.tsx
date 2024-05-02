6dcd418bee17e80796a12090626ed4b18d989d39
mss-souloftware <sufyan.souloftware@gmail.com>
2024-05-02 18:34:09 +0500

    captain - boat details

--- src/components/Tables/TableOne.tsx
+++ src/components/Tables/TableOne.tsx
@@ -5,42 +5,32 @@ const brandData: BRAND[] = [
   {
     logo: "/images/brand/brand-01.svg",
     name: "Google",
-    visitors: 3.5,
+    visitors: "3.5",
     revenues: "5,768",
-    sales: 590,
-    conversion: 4.8,
   },
   {
     logo: "/images/brand/brand-02.svg",
     name: "Twitter",
-    visitors: 2.2,
+    visitors: "2.2",
     revenues: "4,635",
-    sales: 467,
-    conversion: 4.3,
   },
   {
     logo: "/images/brand/brand-03.svg",
     name: "Github",
-    visitors: 2.1,
+    visitors: "2.1",
     revenues: "4,290",
-    sales: 420,
-    conversion: 3.7,
   },
   {
     logo: "/images/brand/brand-04.svg",
     name: "Vimeo",
-    visitors: 1.5,
+    visitors: "1.5",
     revenues: "3,580",
-    sales: 389,
-    conversion: 2.5,
   },
   {
     logo: "/images/brand/brand-05.svg",
     name: "Facebook",
-    visitors: 3.5,
+    visitors: "3.5",
     revenues: "6,768",
-    sales: 390,
-    conversion: 4.2,
   },
 ];
 
@@ -107,11 +97,11 @@ const TableOne = () => {
             </div>
 
             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-black dark:text-white">{brand.sales}</p>
+              {/* <p className="text-black dark:text-white">{brand.sales}</p> */}
             </div>
 
             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-meta-5">{brand.conversion}%</p>
+              {/* <p className="text-meta-5">{brand.conversion}%</p> */}
             </div>
           </div>
         ))}
