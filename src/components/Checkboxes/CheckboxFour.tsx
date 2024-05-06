cf5b20666d8ff4fc2799e9917d23eb0924e1a479
mss-souloftware <sufyan.souloftware@gmail.com>
2024-05-06 19:00:15 +0500

    frontend complete

--- src/components/Checkboxes/CheckboxFour.tsx
+++ src/components/Checkboxes/CheckboxFour.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const CheckboxFour = () => {
+const CheckboxFour = ({ title }) => {
   const [isChecked, setIsChecked] = useState<boolean>(false);
 
   return (
@@ -19,20 +19,18 @@ const CheckboxFour = () => {
             }}
           />
           <div
-            className={`mr-4 flex h-5 w-5 items-center justify-center rounded-full border ${
-              isChecked && "border-primary"
-            }`}
+            className={`mr-4 flex h-5 w-5 items-center justify-center rounded-full border ${isChecked && "border-primary"
+              }`}
           >
             <span
-              className={`h-2.5 w-2.5 rounded-full bg-transparent ${
-                isChecked && "!bg-primary"
-              }`}
+              className={`h-2.5 w-2.5 rounded-full bg-transparent ${isChecked && "!bg-primary"
+                }`}
             >
               {" "}
             </span>
           </div>
         </div>
-        Checkbox Text
+        {title}
       </label>
     </div>
   );
