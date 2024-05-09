56046fb6faf2a6361ccbcf3d90d6db7ef47d30e7
mss-souloftware <sufyan.souloftware@gmail.com>
2024-05-09 14:59:22 +0500

    login API endpoint

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors());
-app.use(cors({
-    origin: 'https://boat-dashboard.vercel.app'
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: 'https://boat-dashboard.vercel.app'
+// }));
 app.use(bodyParser.json());
 
 // MongoDB connection
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 const User = mongoose.model('User', { username: String, password: String });
 
 // Login endpoint
-app.post('/', async (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     // Find user in the database
