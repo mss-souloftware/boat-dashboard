3b6cc8b2b5494d864aa8a6eedfacada6bde106da
mss-souloftware <sufyan.souloftware@gmail.com>
2024-05-09 14:37:40 +0500

    setting up CORS

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: 'https://boat-dashboard.vercel.app'
+}));
 app.use(bodyParser.json());
 
 // MongoDB connection
