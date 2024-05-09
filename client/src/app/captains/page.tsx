5cfbf4a5802289516ab32043ea70d33e66729db4
mss-souloftware <sufyan.souloftware@gmail.com>
2024-05-09 16:56:27 +0500

    api updates

--- client/src/app/captains/page.tsx
+++ client/src/app/captains/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   useEffect(() => {
     const fetchCaptains = async () => {
       try {
-        const response = await axios.get('/captains');
+        const response = await axios.get('https://boat-server.vercel.app//captains');
         setCaptains(response.data);
       } catch (error) {
         console.error('Error fetching captains:', error);
